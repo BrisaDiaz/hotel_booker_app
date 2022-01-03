0c7f2224dc505e26a48a092e5933c8102dce95c7
Brisa Diaz <brisaabigaildiaz2000@gmail.com>
2022-01-03 20:30:50 -0300

    .

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -5,10 +5,10 @@ import {
   NormalizedCacheObject,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-
+import env from '@/env';
 const isBrowser = typeof window !== undefined;
 const httpLink = createHttpLink({
-  uri: `/api/graphql`,
+  uri: `${env.HOST}/api/graphql`,
 
   credentials: 'include',
 });
